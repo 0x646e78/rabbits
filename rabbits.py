101c329f148ee61b2d3a20466a6cffe634fde0f1
auraltension <auraltension@riseup.net>
2013-05-11 19:40:06 +1000

    messing with rabbits

--- rabbits.py
+++ rabbits.py
@@ -1,4 +1,5 @@
 from sys import argv
+from collections import defaultdict
 import chars
 
 script, word = argv
@@ -7,12 +8,34 @@ count = 0
 characters = list(word)
 length = len(characters)
 results = 1
+word_dict = defaultdict(list)
+max_values = []
+counter = [0 for i in range(length)]
+
 
 for i in range(length):
     char = characters[i]
     subs = getattr(chars, char)
-    results = results * len(subs)
-    print char, len(subs), results
+    max_values.append(len(subs))
+    word_dict[i].append(subs)
+
+def build_word():
+    for j in range(length):
+        sub_count = max_values[j]
+        char = chacters[j]
+        while sub_count > 0:
+            position = sub_count -1
+            word_dict = defaultdict[char].append(subs[j]
+            iterate(position)
+            sub_count = sub_count - 1
+#            print ""
+            recursive(x)
+        else:
+            print "Finished"
+
+ 
+#    results = results * len(subs)
+#    print char, len(subs), results
 
 def iterate(position):
     char = characters[position]
