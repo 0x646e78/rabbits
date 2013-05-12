89cb9c4b5e55030916f13257bb40b332e660fbb6
auraltension <auraltension@revoltingworld.net>
2013-05-12 12:31:49 +1000

    Print function works now

--- rabbits.py
+++ rabbits.py
@@ -4,54 +4,77 @@ import chars
 
 script, word = argv
 
-count = 0
 characters = list(word)
 length = len(characters)
-results = 1
 word_dict = defaultdict(list)
-max_values = []
+max_values = [0 for i in range(length)]
 counter = [0 for i in range(length)]
 
+def printit():
+    outword = []
+    count = 0
+    for k in counter:
+        pos = word_dict[count]
+        outword.append(pos[0][k])
+        count += 1
+    print "".join([str(j) for j in outword])
 
-for i in range(length):
-    char = characters[i]
-    subs = getattr(chars, char)
-    max_values.append(len(subs))
-    word_dict[i].append(subs)
+def iterator():
+    global counter
+    for i in range(length):
+        print "i is ", i
+        print "max_values %d" % max_values[i]
+        for j in range(0, max_values[i]):
+            printit()
+            counter[i] = j
 
 def build_word():
-    for j in range(length):
-        sub_count = max_values[j]
-        char = chacters[j]
-        while sub_count > 0:
-            position = sub_count -1
-            word_dict = defaultdict[char].append(subs[j]
-            iterate(position)
-            sub_count = sub_count - 1
-#            print ""
-            recursive(x)
-        else:
-            print "Finished"
+    global max_values
+    for i in range(length):
+        char = characters[i]
+        subs = getattr(chars, char)
+        max_values[i] = len(subs)
+        word_dict[i].append(subs)
+    iterator()
 
- 
-#    results = results * len(subs)
-#    print char, len(subs), results
+build_word()
 
-def iterate(position):
-    char = characters[position]
-    subs = getattr(chars, char)
-    for i in range(len(subs)):
-        this = subs[i]
-#        print this
 
-def recursive(x):
-    if x > 0:
-        position = x -1
-        iterate(position)
-        x = x - 1
-#        print ""
-        recursive(x)
-    else:
-        print "Finished"
 
-recursive(length)
+
+#def build_word():
+#    for j in range(length):
+#        sub_count = len(max_values[j])
+#        char = chacters[j]
+#        while sub_count > 0:
+#            position = sub_count -1
+#            word_dict = defaultdict[char].append(subs[j]
+#                    #            iterate(position)
+#                    #       sub_count = sub_count - 1
+##            print ""
+#            recursive(x)
+#        else:
+#            print "Finished"
+#
+# 
+##    results = results * len(subs)
+##    print char, len(subs), results
+#
+#def iterate(position):
+#    char = characters[position]
+#    subs = getattr(chars, char)
+#    for i in range(len(subs)):
+#        this = subs[i]
+##        print this
+#
+#def recursive(x):
+#    if x > 0:
+#        position = x -1
+#        iterate(position)
+#        x = x - 1
+##        print ""
+#        recursive(x)
+#    else:
+#        print "Finished"
+#
+##recursive(length)
