873a838c49a5835e77345f746c3264061b9dc6b5
auraltension <auraltension@revoltingworld.net>
2013-06-12 18:35:08 +1000

    Output file, verbosity, various tweaks

--- rabbits.py
+++ rabbits.py
@@ -2,21 +2,25 @@
 
 import argparse
 import hashlib
+from os import sys
 from collections import defaultdict
 import chars
 
-# TODO: Argpass arguments to add:
-# --hash <sha1, ntlm> --infile  --outfile  --verbose --mongo
+# Get options
+# TODO:
+# --hash <sha1, ntlm> --infile --mongo --random
 parser = argparse.ArgumentParser(
                                 description='Rabbits is a word permuter. Go forward and multiply',
                                 epilog='''
                                         Written by auraltension.
-                                        "Do what thou wilt shall be the whole of the Law"''')
-parser.add_argument("-s", "--signature", help="Generate commonly used hashes of each permutation",
-                    choices=["md5", "sha1"])
-parser.add_argument("-o", "--of", metavar="file", help="Output file")
-parser.add_argument("-v", "--verbose", action="store_true", help="More verbosity")
-parser.add_argument("word", help="The input string to convert")
+                                        "Do what thou wilt shall be the whole of the Law"
+                                        ''')
+parser.add_argument('-i', '--infile', metavar='filename', help='Input file, one word per line')
+parser.add_argument('-o', '--outfile', metavar='filename', help='Output file')
+parser.add_argument('-s', '--signature', metavar='md5,sha1', help='Generate commonly used hashes of each permutation')
+parser.add_argument('-v', '--verbose', action='store_true', help='Verbosity')
+parser.add_argument('-V', '--verboseplus', action='store_true', help='More verbosity')
+parser.add_argument('word', nargs='?', help="The input string to convert")
 args = parser.parse_args()
 
 # Set up initial state
@@ -26,12 +30,54 @@ word_dict = defaultdict(list)
 max_values = [0 for i in range(length)]
 counter = [0 for i in range(length)]
 
+# print messages if verbosity set
 def verbose(message):
-    if args.verbose:
+    if args.verbose or args.verboseplus:
         print message
 
+# Handle file opening and closing
+def file_op(action):
+    if args.infile:
+        global infile
+        try:
+            if action == 'open':
+                infile = open(args.infile, 'r')
+                verbose("%s opened as input file" % args.infile)
+            if infile and action == 'close':
+                infile.close()
+                verbose("Closing file %s" % args.infile)
+        except (IOError, NameError):
+            sys.exit("Could not open input file.  Exiting")
+    if args.outfile:
+        global outfile
+        if action == 'open':
+            outfile = open(args.outfile, 'w')
+            verbose("%s opened as output file" % args.outfile)
+        if action == 'close':
+            outfile.close()
+            verbose("Closing file %s" % args.outfile)
+
+def output(outword):
+    if args.signature:
+        if "md5" in args.signature:
+            word_md5 = hashlib.md5(outword).hexdigest()
+            printed = outword, word_md5
+            printed = "\t".join([str(column) for column in printed])
+#        if "sha1" in args.signature:
+#            word_sha1 = hashlib.sha1(outword).hexdigest()
+#            plussha1 = outword, word_sha1
+#            printed = "\t".join([str(column) for column in printed])
+    else:
+        printed = outword
+    if args.outfile:
+        if args.verboseplus:
+            verbose(printed)
+        outfile.write("%s\n" % printed)
+    else:
+        print printed
+
 # Map to the output word, perform hashing, print
-def printit():
+def make_permute():
     outword = []
     count = 0
     for k in counter:
@@ -39,24 +85,13 @@ def printit():
         outword.append(pos[0][k])
         count += 1
     outword = "".join([str(j) for j in outword])
-    if args.signature and "md5" in args.signature:
-        word_md5 = hashlib.md5(outword).hexdigest()
-        printed = outword, word_md5
-        printed = "\t".join([str(column) for column in printed])
-    else:
-        printed = outword
-    if args.of:
-        #write to file
-        #verbose?
-        pass
-    else:
-        print printed
+    output(outword)
 
 # Iterate through each positional count
 def iterator():
     global counter
     while True:
-        printit()
+        make_permute()
         for i in range (length-1, -1, -1):
             counter[i] += 1
             if counter[i] < max_values[i]:
@@ -64,20 +99,30 @@ def iterator():
             counter[i] = 0
         if sum(counter) == 0:
             break
+    verbose("Finished permutation output of '%s'" % word)
 
-# Get our possible substitutions, build a dictionary
+# Get our possible letter substitutions, build a dictionary
 def build_word():
     global max_values
+    count = 1
     for i in range(length):
         char = word[i]
         subs = getattr(chars, char)
         max_values[i] = len(subs)
         word_dict[i].append(subs)
+        count = max_values[i] * count
+    verbose("%s possible permutations calculated" % count)
     iterator()
 
+# Start
 try:
+    file_op('open')
+#    for line in infile:
+#        print line
     verbose("Building permutations of '%s'" % word)
     build_word()
 except KeyboardInterrupt:
     print "\nUser inerrupted operation.  Exiting\n"
+finally:
+    file_op('close')
 
